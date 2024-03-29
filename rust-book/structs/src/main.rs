dc25ec0bc20dd8b7d96ed1598c966934a9f34695
Felipe Pessina <fs.pessina@gmail.com>
2024-03-29 04:05:06 +0400

    Struct example

--- rust-book/structs/src/main.rs
+++ rust-book/structs/src/main.rs
@@ -1,30 +1,41 @@
-struct User {
-    name: String,
-    is_retired: bool,
-    age: u32,
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+    fn can_hold(&self, target: &Rectangle) -> bool {
+        self.width > target.width && self.height > target.height
+    }
+    fn square(dimension: u32) -> Self {
+        Self {
+            width: dimension,
+            height: dimension,
+        }
+    }
 }
 
 fn main() {
-    let mut felipe = User {
-        name: "Felipe".to_string(),
-        is_retired: false,
-        age: 32,
+    let rect = Rectangle {
+        height: 32,
+        width: 32,
+    };
+    let rect2 = Rectangle {
+        height: 24,
+        width: 24,
+    };
+    let rect3 = Rectangle {
+        height: 24,
+        width: 33,
     };
 
-    let user2 = build_user(String::from("Felipe"), 32);
+    let square = Rectangle::square(2);
 
-    let user3 = User {
-        name: "Alberto".to_string(),
-        ..felipe
-    };
-
-    felipe.name = "Isabela".to_string();
-}
-
-fn build_user(name: String, age: u32) -> User {
-    User {
-        name,
-        age,
-        is_retired: false,
-    }
+    println!("{}", rect.area());
+    println!("{}", rect.can_hold(&rect2));
+    println!("{}", rect.can_hold(&rect3));
+    println!("{}", square.area());
 }
