1ed0cbb8d1561d383ee768ccdfaadf0ef8a5c22a
Pessina <fs.pessina@gmail.com>
2024-04-14 08:36:46 +0300

    Project IO done

--- rust-book/minigrep/src/lib.rs
+++ rust-book/minigrep/src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,14 +15,84 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(config.file_path)?;
 
-    println!("File content: {}", file_content);
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &file_content)
+    } else {
+        search(&config.query, &file_content)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut ret = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            ret.push(line)
+        }
+    }
+
+    ret
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut vec = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            vec.push(line)
+        }
+    }
+
+    return vec;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust
+safe, fast, productive
+Pick three.
+Duct tape";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RuST";
+        let contents = "\
+RuSt
+safe, fast, productive
+Pick three.
+Trust me";
+
+        assert_eq!(
+            vec!["RuSt", "Trust me"],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- rust-book/minigrep/src/main.rs
+++ rust-book/minigrep/src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error reading args: {err}");
+        eprintln!("Error reading args: {err}");
         process::exit(1)
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
