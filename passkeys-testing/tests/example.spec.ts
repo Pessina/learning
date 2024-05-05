1a71380e79729b73d17c127b141060494264e663
Felipe Pessina <fs.pessina@gmail.com>
2024-05-05 21:07:15 +0700

    Add more comments with description

--- passkeys-testing/tests/example.spec.ts
+++ passkeys-testing/tests/example.spec.ts
@@ -2,56 +2,43 @@ import { faker } from "@faker-js/faker";
 import { test, expect, CDPSession } from "@playwright/test";
 import crypto from "crypto";
 
-let client: CDPSession;
-let authenticatorId: string;
-
-// test.beforeEach(async ({ page }) => {
-//   client = await page.context().newCDPSession(page);
-//   await client.send("WebAuthn.enable", { enableUI: true });
-
-//   const result = await client.send("WebAuthn.addVirtualAuthenticator", {
-//     options: {
-//       protocol: "ctap2",
-//       transport: "internal",
-//       hasResidentKey: true,
-//       hasUserVerification: false,
-//       isUserVerified: true,
-//       automaticPresenceSimulation: false,
-//     },
-//   });
-//   authenticatorId = result.authenticatorId;
-
-//   await page.goto("https://passkeys.io");
-// });
-
-// test.afterEach(async () => {
-//   await client.send("WebAuthn.clearCredentials", { authenticatorId });
-
-//   await client.send("WebAuthn.removeVirtualAuthenticator", { authenticatorId });
-// });
-
-test("credential creation and retrieval", async ({ page }) => {
-  const credentialId = await createPasskey({
-    client: client,
-    email: "test@gmail.com",
-    authenticatorId: authenticatorId,
-    rpId: "www.passkeys.io",
-  });
-
-  await page.getByRole("button", { name: "Sign in with a passkey" }).click();
-
-  const retrievedCredential = await client.send("WebAuthn.getCredential", {
-    authenticatorId,
-    credentialId,
-  });
-
-  console.log("Retrieved Credential:", retrievedCredential);
-});
-
 test("test2", async ({ page }) => {
   const email = faker.internet.email();
   await page.goto("https://passkeys.io");
+  const { client, authenticatorId } = await setupVirtualAuthenticator(page);
 
+  await page.getByRole("textbox", { name: "Email" }).fill(email);
+  await page.getByRole("button", { name: "Continue" }).click();
+  await page.getByRole("button", { name: "Sign up" }).click();
+
+  // Add one credential
+  await createPasskey({
+    client: client,
+    email,
+    authenticatorId: authenticatorId,
+    rpId: "passkeys.io",
+  });
+
+  // Add one credential
+  // TODO: Check how to re-use the credential created above for creation of passkey so we can have a fixed user for all testing purpose.
+  // Maybe we can use a unique email and use recovery for testing, but not optimal coz we have to always call the recovery. Ideal it's have a hard-coded value on FE
+  await page.getByRole("button", { name: "Create a passkey" }).click();
+
+  await expect(
+    page.getByText("Used for passcode authentication.")
+  ).toBeVisible();
+
+  const credentials = await client.send("WebAuthn.getCredentials", {
+    authenticatorId,
+  });
+
+  // Prints two credential
+  for (const c of credentials.credentials) {
+    console.log(c);
+  }
+});
+
+async function setupVirtualAuthenticator(page) {
   const client = await page.context().newCDPSession(page);
   // Disable UI for automated testing
   await client.send("WebAuthn.enable", { enableUI: false });
@@ -63,8 +50,6 @@ test("test2", async ({ page }) => {
       hasResidentKey: true,
       hasUserVerification: true,
       isUserVerified: true,
-      // Necessary to trigger passkey automatically.
-      // Not clear what I have to do in case I set it to false, I have to basically manually submit the passkeys
       automaticPresenceSimulation: true,
     },
   });
@@ -77,31 +62,8 @@ test("test2", async ({ page }) => {
     console.log("WebAuthn.credentialAsserted");
   });
 
-  await page.getByRole("textbox", { name: "Email" }).fill(email);
-  await page.getByRole("button", { name: "Continue" }).click();
-  await page.getByRole("button", { name: "Sign up" }).click();
-
-  await createPasskey({
-    client: client,
-    email,
-    authenticatorId: authenticatorId,
-    rpId: "passkeys.io",
-  });
-
-  await page.getByRole("button", { name: "Create a passkey" }).click();
-
-  await expect(
-    page.getByText("Used for passcode authentication.")
-  ).toBeVisible();
-
-  const credentials = await client.send("WebAuthn.getCredentials", {
-    authenticatorId,
-  });
-
-  for (const c of credentials.credentials) {
-    console.log(c);
-  }
-});
+  return { client, authenticatorId };
+}
 
 async function createPasskey({
   client,
