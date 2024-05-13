67fa86fa6bb122b78337da13b5d2914065d5ab2a
Felipe Pessina <fs.pessina@gmail.com>
2024-05-13 19:21:33 +0700

    Closures

--- rust-book/closure/src/main.rs
+++ rust-book/closure/src/main.rs
@@ -1,110 +1,31 @@
-use std::thread;
+use std::borrow::Borrow;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-enum ShirtColor {
-    Red,
-    Blue,
+fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
-struct Inventory {
-    shirts: Vec<ShirtColor>,
+fn do_twice(f: fn(i32) -> i32, x: i32) -> i32 {
+    f(x) + f(x)
 }
 
-impl Inventory {
-    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
-        user_preference.unwrap_or_else(|| self.most_stocked())
-    }
-
-    fn most_stocked(&self) -> ShirtColor {
-        let mut red = 0;
-        let mut blue = 0;
-
-        for s in &self.shirts {
-            match s {
-                ShirtColor::Red => red += 1,
-                &ShirtColor::Blue => blue += 1,
-            }
-        }
-
-        if red > blue {
-            ShirtColor::Red
-        } else {
-            ShirtColor::Blue
-        }
-    }
+enum Status {
+    Value(i32),
+    None,
 }
 
-struct Rectangle {
-    width: u32,
-    height: u32,
+fn get_func() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| 10)
 }
 
 fn main() {
-    let store = Inventory {
-        shirts: vec![ShirtColor::Red, ShirtColor::Blue, ShirtColor::Red],
-    };
+    println!("Do twice result {}", do_twice(add_one, 2));
+    println!("Do twice result {}", do_twice(|x: i32| { x * 10 }, 2));
 
-    let user_pref1 = Some(ShirtColor::Blue);
-    let giveaway = store.giveaway(user_pref1);
+    let v = vec![1, 2, 3, 4, 5];
+    let b: Vec<Status> = (0..10).map(Status::Value).collect();
 
-    println!("{:?}", giveaway);
+    let f = get_func();
 
-    let user_pref2 = None;
-    let giveaway = store.giveaway(user_pref2);
-
-    println!("{:?}", giveaway);
-
-    let add_one_v4 = |x| x + 1;
-    let x = add_one_v4(1);
-
-    let example_closure = |x| x;
-    let x = example_closure(String::from("3"));
-
-    let only_borrow = || println!("{x}");
-
-    println!("{x}");
-    only_borrow();
-    println!("{x}");
-
-    let mut list = String::new();
-    let mut borrow_mutate = || list.push('3');
-
-    // println!("{list}");
-    borrow_mutate();
-    println!("{list}");
-
-    let list = vec![1, 2, 3];
-
-    println!("Before defining closure: {:?}", list);
-
-    thread::spawn(move || println!("From thread: {:?}", list))
-        .join()
-        .unwrap();
-
-    // println!("Before defining closure: {:?}", list);
-
-    let mut rect_list = [
-        Rectangle {
-            width: 8,
-            height: 8,
-        },
-        Rectangle {
-            width: 12,
-            height: 64,
-        },
-        Rectangle {
-            width: 15,
-            height: 32,
-        },
-    ];
-
-    let mut count_rect = 0;
-    // let mut count_list = vec![];
-    // let my_str = String::from("count");
-
-    rect_list.sort_by_key(|r| {
-        // count_list.push(my_str);
-        count_rect += 1;
-        r.width
-    });
+    println!("Get func result {}", f(10));
+    println!("Get func result {}", get_func()(10));
 }
