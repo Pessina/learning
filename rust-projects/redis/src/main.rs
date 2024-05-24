9b9667618e99121361773be554ea6c5acd195f37
Felipe Pessina <fs.pessina@gmail.com>
2024-05-24 22:02:04 +0700

    Improve code

--- rust-projects/redis/src/main.rs
+++ rust-projects/redis/src/main.rs
@@ -1,11 +1,11 @@
 use redis::modules::server::handle_connection;
-use std::{io::Read, net::TcpListener};
+use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for wrapped_stream in listener.incoming() {
-        let mut stream = wrapped_stream.unwrap();
+        let stream = wrapped_stream.unwrap();
         handle_connection(stream);
     }
 }
