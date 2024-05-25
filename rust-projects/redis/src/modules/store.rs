f020dfb05da1d9d3d3596423bcff6c1e3fd1bead
Felipe Pessina <fs.pessina@gmail.com>
2024-05-25 19:50:08 +0700

    Add tests for Redis

--- rust-projects/redis/src/modules/store.rs
+++ rust-projects/redis/src/modules/store.rs
@@ -19,3 +19,35 @@ impl Redis {
         self.map.get(key)
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_succeed_get() {
+        let mut redis = Redis::new();
+        redis.set("Name".to_string(), "Felipe".to_string());
+        let name = redis.get("Name").unwrap();
+        assert_eq!(name.to_owned(), "Felipe".to_string());
+    }
+
+    #[test]
+    fn it_should_fail_get() {
+        let mut redis = Redis::new();
+        redis.set("Name".to_string(), "Felipe".to_string());
+        match redis.get("Age") {
+            None => assert!(true),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn it_should_overwrite_insert() {
+        let mut redis = Redis::new();
+        redis.set("Name".to_string(), "Felipe".to_string());
+        redis.set("Name".to_string(), "Carlos".to_string());
+        let name = redis.get("Name").unwrap();
+        assert_eq!(name.to_owned(), "Carlos".to_string());
+    }
+}
