a62302602950e500a408e3fd36e6c05e03db9251
Pessina <fs.pessina@gmail.com>
2024-07-05 17:22:20 +0700

    FrontRunning issue fixed

--- rust-smart-contracts/crossword-contract/src/lib.rs
+++ rust-smart-contracts/crossword-contract/src/lib.rs
@@ -1,10 +1,35 @@
 use near_sdk::{
-    collections::UnorderedSet, env, log, near, serde_json, store::LookupMap, AccountId, NearToken,
-    PanicOnDefault, Promise,
+    collections::UnorderedSet, env, ext_contract, log, near, serde_json, store::LookupMap,
+    AccountId, Allowance, Gas, NearToken, PanicOnDefault, Promise, PromiseResult, PublicKey,
 };
 
-const PUZZLE_NUMBER: u8 = 1;
 const PRIZE_AMOUNT: NearToken = NearToken::from_near(1);
+const GAS_FOR_ACCOUNT_CREATION: Gas = Gas::from_gas(150_000_000_000_000);
+const GAS_FOR_ACCOUNT_CALLBACK: Gas = Gas::from_gas(110_000_000_000_000);
+
+#[ext_contract(ext_linkdrop)]
+pub trait ExtLinkDropCrossContract {
+    fn create_account(&mut self, new_account_id: AccountId, new_pk: PublicKey) -> Promise;
+}
+
+pub trait AfterClaim {
+    fn call_back_after_create_account(
+        &mut self,
+        crossword_pk: PublicKey,
+        new_acc_id: AccountId,
+        memo: String,
+        signer_account_pk: PublicKey,
+    ) -> bool;
+
+    fn call_back_after_transfer(
+        &mut self,
+        crossword_pk: PublicKey,
+        receiver_acc_id: AccountId,
+        memo: String,
+        signer_account_pk: PublicKey,
+    ) -> bool;
+}
+
 #[near(serializers = [json, borsh])]
 pub struct JsonPuzzle {
     solution_hash: String,
@@ -39,8 +64,9 @@ pub struct Answer {
 #[near(serializers = [json, borsh])]
 #[derive(Clone)]
 pub enum PuzzleStatus {
-    Solved { memo: String },
+    Solved { solver_pk: PublicKey },
     Unsolved,
+    Claimed { memo: String },
 }
 
 #[near(serializers = [json, borsh])]
@@ -54,31 +80,34 @@ struct Puzzle {
 #[derive(PanicOnDefault)]
 pub struct Crossword {
     owner_id: AccountId,
-    puzzles: LookupMap<String, Puzzle>,
-    unsolved_puzzles: UnorderedSet<String>,
+    puzzles: LookupMap<PublicKey, Puzzle>,
+    unsolved_puzzles: UnorderedSet<PublicKey>,
+    creator_account: AccountId,
 }
 
 #[near]
 impl Crossword {
     #[init(ignore_state)]
-    pub fn migrate(owner_id: AccountId) -> Self {
+    pub fn migrate(owner_id: AccountId, creator_account: AccountId) -> Self {
         Self {
             owner_id,
-            puzzles: LookupMap::new(b"c"),
-            unsolved_puzzles: UnorderedSet::new(b"u"),
+            puzzles: LookupMap::new(b"a"),
+            unsolved_puzzles: UnorderedSet::new(b"b"),
+            creator_account,
         }
     }
 
     #[init]
-    pub fn new(owner_id: AccountId) -> Self {
+    pub fn new(owner_id: AccountId, creator_account: AccountId) -> Self {
         Self {
             owner_id,
             puzzles: LookupMap::new(b"p"),
             unsolved_puzzles: UnorderedSet::new(b"u"),
+            creator_account,
         }
     }
 
-    pub fn new_puzzle(&mut self, solution_hash: String, answers: Vec<Answer>) {
+    pub fn new_puzzle(&mut self, public_key: PublicKey, answers: Vec<Answer>) {
         assert_eq!(
             env::predecessor_account_id(),
             self.owner_id,
@@ -86,7 +115,7 @@ impl Crossword {
         );
 
         let existing = self.puzzles.insert(
-            solution_hash.clone(),
+            public_key.clone(),
             Puzzle {
                 status: PuzzleStatus::Unsolved,
                 answers,
@@ -94,42 +123,204 @@ impl Crossword {
         );
 
         assert!(existing.is_none(), "Puzzle with that key already exists");
-        self.unsolved_puzzles.insert(&solution_hash);
+        self.unsolved_puzzles.insert(&public_key);
     }
 
-    pub fn submit_solution(&mut self, solution: String, memo: String) -> Promise {
-        let hash_input = env::sha256(solution.as_bytes());
-        let hashed_input_hex = hex::encode(hash_input);
+    pub fn submit_solution(&mut self, memo: String, solver_pk: PublicKey) -> Promise {
+        // Old hashing code for solution
+        // let hash_input = env::sha256(solution.as_bytes());
+        // let hashed_input_hex = hex::encode(hash_input);
+
+        let answer_pk = env::signer_account_pk();
 
         let mut puzzle = self
             .puzzles
-            .get(&hashed_input_hex)
+            .get(&answer_pk)
             .expect("ERR_NO_CORRECT_ANSWER")
             .clone();
 
         puzzle.status = match puzzle.status {
-            PuzzleStatus::Unsolved => PuzzleStatus::Solved { memo: memo.clone() },
+            PuzzleStatus::Unsolved => PuzzleStatus::Solved {
+                solver_pk: solver_pk.clone(),
+            },
             _ => env::panic_str("ERR_PUZZLE_SOLVED"),
         };
 
-        self.puzzles.insert(hashed_input_hex.clone(), puzzle);
-        self.unsolved_puzzles.remove(&hashed_input_hex);
+        self.puzzles.insert(answer_pk.clone(), puzzle);
+        self.unsolved_puzzles.remove(&answer_pk);
 
         log!(
-            "Puzzle with solution hash {} solved, with memo: {}",
-            hashed_input_hex,
+            "Puzzle with solution hash {:?} solved, with memo: {}",
+            answer_pk,
             memo
         );
 
-        Promise::new(env::predecessor_account_id()).transfer(PRIZE_AMOUNT)
+        Promise::new(env::current_account_id()).add_access_key_allowance(
+            solver_pk.into(),
+            Allowance::limited(NearToken::from_yoctonear(250000000000000000000000)).unwrap(),
+            env::current_account_id(),
+            "claim_reward,claim_reward_new_account".to_string(),
+        );
+
+        Promise::new(env::current_account_id()).delete_key(answer_pk)
     }
 
-    pub fn get_puzzle_status(&self, solution_hash: String) -> &PuzzleStatus {
+    pub fn claim_reward_new_account(
+        &self,
+        crossword_pk: PublicKey,
+        new_acc_id: AccountId,
+        new_pk: PublicKey,
+        memo: String,
+    ) -> Promise {
+        let signer_pk = env::signer_account_pk();
+
+        let puzzle = self
+            .puzzles
+            .get(&crossword_pk)
+            .expect("Puzzle doesn't exist");
+
+        match puzzle.status {
+            PuzzleStatus::Solved {
+                solver_pk: ref puzzle_pk,
+            } => {
+                assert_eq!(puzzle_pk.clone(), signer_pk, "You're not the person who can claim this, or else you need to use your function-call access key, friend");
+            }
+            _ => env::panic_str("Puzzle should have `Solved` status to be claimed."),
+        }
+
+        ext_linkdrop::ext(self.creator_account.clone())
+            .with_attached_deposit(PRIZE_AMOUNT)
+            .with_static_gas(GAS_FOR_ACCOUNT_CREATION)
+            .create_account(new_acc_id.clone(), new_pk)
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(GAS_FOR_ACCOUNT_CALLBACK)
+                    .call_back_after_create_account(
+                        crossword_pk,
+                        new_acc_id,
+                        memo,
+                        env::signer_account_pk(),
+                    ),
+            )
+    }
+
+    pub fn claim_reward(
+        &self,
+        crossword_pk: PublicKey,
+        receiver_acc_id: AccountId,
+        memo: String,
+    ) -> Promise {
+        let signer_pk = env::signer_account_pk();
+
+        let puzzle = self
+            .puzzles
+            .get(&crossword_pk)
+            .expect("That puzzle doesn't exist");
+
+        match puzzle.status {
+            PuzzleStatus::Solved {
+                solver_pk: ref puzzle_pk,
+            } => {
+                assert_eq!(puzzle_pk.clone(), signer_pk, "You're not the person who can claim this, or else you need to use your function-call access key, friend");
+            }
+            _ => env::panic_str("Puzzle should have `Solved` status to be claimed."),
+        };
+
+        Promise::new(receiver_acc_id.clone())
+            .transfer(PRIZE_AMOUNT)
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(GAS_FOR_ACCOUNT_CALLBACK)
+                    .call_back_after_transfer(
+                        crossword_pk,
+                        receiver_acc_id,
+                        memo,
+                        env::signer_account_pk(),
+                    ),
+            )
+    }
+
+    pub fn get_puzzle_status(&self, answer_pk: PublicKey) -> &PuzzleStatus {
         &self
             .puzzles
-            .get(&solution_hash)
+            .get(&answer_pk)
             .expect("Puzzle doesn't exist")
             .status
+    }
+
+    pub fn finalize_puzzle(
+        &mut self,
+        crossword_pk: PublicKey,
+        account_id: AccountId,
+        memo: String,
+        signer_pk: PublicKey,
+    ) {
+        let mut puzzle = self
+            .puzzles
+            .get(&crossword_pk)
+            .expect("Puzzle doesn't exist")
+            .clone();
+
+        puzzle.status = PuzzleStatus::Claimed { memo: memo.clone() };
+
+        self.puzzles.insert(crossword_pk.clone(), puzzle);
+
+        log!(
+            "Puzzle with pk: {:?} claimed, new account created: {}, memo: {}, reward claimed: {}",
+            crossword_pk,
+            account_id,
+            memo,
+            PRIZE_AMOUNT
+        );
+
+        Promise::new(env::current_account_id()).delete_key(signer_pk);
+    }
+}
+
+#[near]
+impl AfterClaim for Crossword {
+    #[private]
+    fn call_back_after_create_account(
+        &mut self,
+        crossword_pk: PublicKey,
+        new_acc_id: AccountId,
+        memo: String,
+        signer_account_pk: PublicKey,
+    ) -> bool {
+        assert_eq!(env::promise_results_count(), 1, "Expected 1 promise result");
+
+        match env::promise_result(0) {
+            PromiseResult::Successful(_) => {
+                self.finalize_puzzle(crossword_pk, new_acc_id, memo, signer_account_pk);
+                true
+            }
+            PromiseResult::Failed => false,
+        }
+    }
+
+    #[private]
+    fn call_back_after_transfer(
+        &mut self,
+        crossword_pk: PublicKey,
+        receiver_acc_id: AccountId,
+        memo: String,
+        signer_account_pk: PublicKey,
+    ) -> bool {
+        assert_eq!(env::promise_results_count(), 1, "Expected 1 promise result");
+
+        match env::promise_result(0) {
+            PromiseResult::Successful(creation_result) => {
+                let creation_succeeded: bool = serde_json::from_slice(&creation_result)
+                    .expect("Could not turn result from account creation into boolean");
+                if creation_succeeded {
+                    self.finalize_puzzle(crossword_pk, receiver_acc_id, memo, signer_account_pk);
+                    true
+                } else {
+                    false
+                }
+            }
+            PromiseResult::Failed => todo!(),
+        }
     }
 }
 
