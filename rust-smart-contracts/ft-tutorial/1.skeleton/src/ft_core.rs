372e2346f78656096cf1544bb98082915d7e6776
Pessina <fs.pessina@gmail.com>
2024-07-06 16:32:01 +0700

    Add ft_transfer_call

--- rust-smart-contracts/ft-tutorial/1.skeleton/src/ft_core.rs
+++ rust-smart-contracts/ft-tutorial/1.skeleton/src/ft_core.rs
@@ -1,4 +1,4 @@
-use near_sdk::{assert_one_yocto, ext_contract, log, Gas, PromiseOrValue, PromiseResult};
+use near_sdk::{assert_one_yocto, ext_contract, log, Gas, Promise, PromiseOrValue, PromiseResult};
 
 use crate::*;
 
@@ -78,10 +78,20 @@ impl FungibleTokenCore for Contract {
         memo: Option<String>,
         msg: String,
     ) -> PromiseOrValue<NearToken> {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        self.assert_one_yocto();
+
+        let sender_id = env::predecessor_account_id();
+
+        self.internal_transfer(&sender_id, &receiver_id, amount, memo);
+
+        ext_ft_receiver::ext(receiver_id.clone())
+            .ft_on_transfer(sender_id.clone(), amount.into(), msg)
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(GAS_FOR_RESOLVE_TRANSFER)
+                    .ft_resolve_transfer(&sender_id, receiver_id, amount),
+            )
+            .into()
     }
 
     fn ft_total_supply(&self) -> U128 {
@@ -157,9 +167,37 @@ impl Contract {
         receiver_id: AccountId,
         amount: NearToken,
     ) -> NearToken {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        let unused_amount = match env::promise_result(0) {
+            PromiseResult::Successful(value) => {
+                if let Ok(unused_amount) = near_sdk::serde_json::from_slice::<NearToken>(&value) {
+                    std::cmp::min(amount, unused_amount)
+                } else {
+                    amount
+                }
+            }
+
+            PromiseResult::Failed => amount,
+        };
+
+        if unused_amount.gt(&ZERO_TOKEN) {
+            let receiver_balance = self.accounts.get(&receiver_id).unwrap_or(ZERO_TOKEN);
+            if receiver_balance.gt(&ZERO_TOKEN) {
+                let refund_amount = std::cmp::min(receiver_balance, unused_amount);
+
+                self.internal_transfer(
+                    &receiver_id,
+                    &sender_id,
+                    refund_amount,
+                    Some("Refund".to_string()),
+                );
+
+                let used_amount = amount
+                    .checked_sub(refund_amount)
+                    .unwrap_or_else(|| env::panic_str("Total supply overflow"));
+                return used_amount;
+            }
+        }
+
+        amount
     }
 }
