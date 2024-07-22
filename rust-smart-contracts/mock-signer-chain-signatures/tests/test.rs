b5ad5e8c1b902ad651c489d93ecfb6249f5331d4
Pessina <fs.pessina@gmail.com>
2024-07-22 22:40:05 +0700

    Simplify code

--- rust-smart-contracts/mock-signer-chain-signatures/tests/test.rs
+++ rust-smart-contracts/mock-signer-chain-signatures/tests/test.rs
@@ -1,10 +1,10 @@
 use ethers_core::{
-    k256::elliptic_curve::{group::GroupEncoding, point::AffineCoordinates},
+    k256::elliptic_curve::point::AffineCoordinates,
     types::{Signature, U256},
-    utils::hex::{hex, ToHexExt},
+    utils::hex::ToHexExt,
 };
 
-use k256::{ecdsa::VerifyingKey, pkcs8::DecodePublicKey, AffinePoint};
+use k256::ecdsa::VerifyingKey;
 use mock_signer_chain_signatures::{
     derive_eth_address, derive_public_key,
     lib::types::{SignRequest, SignatureResponse},
@@ -43,48 +43,46 @@ async fn test_contract_sign_request() {
         key_version: 0,
     };
 
-    let status = contract
+    let sign_response: SignatureResponse = contract
         .call("sign")
         .args_json(serde_json::json!(sign_request))
         .deposit(NearToken::from_yoctonear(1))
         .max_gas()
         .transact_async()
         .await
+        .unwrap()
+        .await
+        .unwrap()
+        .json()
         .unwrap();
 
-    let result = status.await.unwrap();
-    let execution = result.into_result().unwrap();
-    let sign_response: SignatureResponse = execution.json().unwrap();
-
-    let status = contract
+    let public_key_response: PublicKey = contract
         .call("public_key")
         .max_gas()
         .transact_async()
         .await
+        .unwrap()
+        .await
+        .unwrap()
+        .json()
         .unwrap();
 
-    let result = status.await.unwrap();
-    let execution = result.into_result().unwrap();
-    let public_key_response: PublicKey = execution.json().unwrap();
-
-    let r_bytes = sign_response.big_r.affine_point.x();
-    let r = U256::from_big_endian(&r_bytes);
+    let r = U256::from_big_endian(&sign_response.big_r.affine_point.x());
     let s = U256::from_big_endian(&sign_response.s.scalar.to_bytes());
     let v = (sign_response.recovery_id + 27) as u64;
-
     let signature = Signature { r, s, v };
 
-    let address = signature.recover(sign_request.payload).unwrap();
+    let recovered_address = signature.recover(sign_request.payload).unwrap();
 
     let root_public_key = String::from(&public_key_response);
-
     let root_public_key = root_public_key.split(":").nth(1).unwrap();
     let root_public_key = bs58::decode(root_public_key).into_vec().unwrap();
     let mut sec1_key = vec![0x04];
     sec1_key.extend_from_slice(&root_public_key);
     let root_public_key = VerifyingKey::from_sec1_bytes(&sec1_key).unwrap();
+
     let public_key = derive_public_key(&root_public_key, predecessor.to_string(), path.to_string());
     let derived_address = derive_eth_address(&public_key);
 
-    assert_eq!(address.encode_hex(), derived_address);
+    assert_eq!(recovered_address.encode_hex(), derived_address);
 }
