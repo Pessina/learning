66a74aa1c421fd7b603fde7aee8f7f318063959c
Pessina <fs.pessina@gmail.com>
2024-07-24 17:21:19 +0700

    Add code to prove the server the Gift is on the list

--- alchemy/eth-bootcamp/GiftList/client/index.js
+++ alchemy/eth-bootcamp/GiftList/client/index.js
@@ -4,11 +4,18 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  let index = Math.floor(Math.random() * niceList.length);
+  let name = niceList[index];
+  let proof = merkleTree.getProof(index)
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof, 
+    name
   });
 
   console.log({ gift });
