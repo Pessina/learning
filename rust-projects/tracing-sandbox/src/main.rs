52beca216b010b9147a0a07a8ccb403ee4b5e7fb
Pessina <fs.pessina@gmail.com>
2024-08-07 14:39:02 +0700

    Update tracing example to conditionally add the stackdriver layer

--- rust-projects/tracing-sandbox/src/main.rs
+++ rust-projects/tracing-sandbox/src/main.rs
@@ -3,28 +3,33 @@ use tracing_stackdriver::layer as stackdriver_layer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 fn main() -> anyhow::Result<()> {
-    // let stackdriver = stackdriver_layer().with_writer(|| std::io::stderr());
+    let stackdriver = stackdriver_layer().with_writer(std::io::stderr);
 
-    // let fmt_layer = tracing_subscriber::fmt::layer()
+    let fmt_layer = tracing_subscriber::fmt::layer().with_thread_ids(true);
+
+    let env_filter = EnvFilter::from_default_env();
+    let base_subscriber = Registry::default().with(env_filter);
+
+    let is_gcp = true;
+    let subscriber = if is_gcp {
+        base_subscriber
+            .with(Some(stackdriver))
+            .with(fmt_layer.with_ansi(false))
+    } else {
+        base_subscriber.with(None).with(fmt_layer.with_ansi(true))
+    };
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+
+    let _span = tracing::trace_span!("main").entered();
+
+    // let subscriber = tracing_subscriber::fmt()
     //     .with_thread_ids(true)
+    //     .with_env_filter(EnvFilter::from_default_env())
     //     .with_ansi(false);
 
-    // let subscriber = Registry::default()
-    //     .with(EnvFilter::from_default_env())
-    //     .with(fmt_layer)
-    //     .with(stackdriver);
-
-    // tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
-
-    // let _span = tracing::trace_span!("main").entered();
-
-    let subscriber = tracing_subscriber::fmt()
-        .with_thread_ids(true)
-        .with_env_filter(EnvFilter::from_default_env())
-        .with_ansi(false);
-
-    subscriber.init();
-    let _span = tracing::trace_span!("cli").entered();
+    // subscriber.init();
+    // let _span = tracing::trace_span!("cli").entered();
 
     info!("User logged in");
     debug!("User session created");
