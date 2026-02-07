84044b4e6fcfbf70dcee0ef874a2992699dab75f
Pessina <fs.pessina@gmail.com>
2026-02-07 16:29:57 +0700

    Add daml examples

--- daml/create-daml-app/daml.js/create-daml-app-0.1.0/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as Setup from './Setup';
-export { Setup } ;
-import * as User from './User';
-export { User } ;
-export declare const packageId = '4e9ada522abf51b008103d9de947b3e96a446af360fd64b771a30a79ae9d02c6';
